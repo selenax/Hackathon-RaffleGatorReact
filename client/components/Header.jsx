68f5b628ba802e2965b62133c2437c60ed17391a
gmhawk <alanfu@live.com>
2018-11-03 17:59:00 -0700

    Added Buttons to Header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import SearchBar from './SearchBar';
 
 class Header extends Component {
   constructor(props) {
@@ -9,11 +14,38 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-        [Header here]
+      <div id="header-container">
+        <AppBar
+          position="static"
+          style={{ background: 'lightblue' }}
+        >
+          <Toolbar color="secondary" style={{ marginLeft: 'auto' }}>
+            <SearchBar />
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ color: 'white', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}
+            >
+Home
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ color: 'white', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}
+            >
+About
+            </Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default Header;
+const styles = {
+  root: {
+    display: 'flex',
+  },
+};
+
+export default withStyles(styles)(Header);
