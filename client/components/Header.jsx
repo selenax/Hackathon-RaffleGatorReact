b99187b0c09b9eca8808b5e70f82261156cc708d
gmhawk <alanfu@live.com>
2018-11-04 00:28:36 -0700

    Added Form button

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -36,6 +36,13 @@ class Header extends Component {
             >
               <Link to="/about">About</Link>
             </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ margin: '10px', color: 'white', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}
+            >
+              <Link to="/form">Form</Link>
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
