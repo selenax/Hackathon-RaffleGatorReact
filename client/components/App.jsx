1f6ab92952fbcf90c386a8e42196b09cf3c8c7c4
Andrew Ngo <andrew@Andrews-MacBook-Pro-3.local>
2018-11-03 20:40:51 -0700

    Includes readAll and addContest queries

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import ContestContainer from './ContestContainer';
 import ContestListing from './ContestListing';
+import readAll from './models/readAll.js';
+import addContest from './models/addContest.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      contests: null
     };
+  }
+
+  componentDidMount() {
+
+    readAll().then((results) => {
+      this.setState({
+        contests: results
+      })
+    })
+
   }
 
   render() {
