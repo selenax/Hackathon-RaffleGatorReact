4f918797d330a13e6515214fe9328881dcb7a8c0
selenax <selenawenxu@gmail.com>
2018-11-04 10:07:17 -0800

    fixed bug on server

--- server.js
+++ server.js
@@ -13,12 +13,8 @@ const app = express();
 
 // const axios = require('axios');
 app.use(cors());
-app.use(express.static(`${__dirname}/../client/dist`));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-  console.log('😊 server connected');
-});
-// app.use('/', express.static('public'));
+
+app.use('/', express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
