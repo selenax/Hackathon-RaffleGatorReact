6e67555b207be49d28777ac8bd9c62d4f8628a4b
gmhawk <alanfu@live.com>
2018-11-03 18:11:46 -0700

    Added new search bar

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -24,7 +24,7 @@ class Header extends Component {
             <Button
               variant="contained"
               color="secondary"
-              style={{ color: 'white', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}
+              style={{ margin: '10px', color: 'white', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}
             >
 Home
             </Button>
